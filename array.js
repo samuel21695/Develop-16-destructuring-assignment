2c941e4c8108b2404d959fc23228192f225aea49
samuel21695 <samuel21695@gmail.com>
2023-10-18 11:01:42 +0900

    array 절차방식 && 선언방식

---
+++ array.js
@@ -0,0 +1,10 @@
+const arrayA = [];
+const arrayB = ["피키츄", "라이츄", "파이리"];
+
+// 절차방식
+for (let i=0; i < arrayB.length; i++) {
+  arrayA[i] = arrayB[i];
+}
+
+// 선언방식
+const arrayC = arrayA.map(data => data);
